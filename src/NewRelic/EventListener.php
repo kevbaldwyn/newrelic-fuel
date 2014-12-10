a38e89c86c2124b1a45dad2f1006e047c3eb8d53
Kevin Baldwyn <kev@kevbaldwyn.co.uk>
2014-12-10 12:51:27 +0000

    refactored event listener

--- src/NewRelic/EventListener.php
+++ src/NewRelic/EventListener.php
@@ -4,13 +4,16 @@ use Fuel\Core\Event;
 
 class EventListener {
 
-    const EVENT_LISTEN = 'controller_started';
+    const EVENT_TRANSACTION = 'controller_started';
+    const EVENT_APP_BOOT    = 'app_created';
 
     public static function register()
     {
         if (extension_loaded('newrelic')) {
-            Event::register(self::EVENT_LISTEN, 'KevBaldwyn\NewRelic\EventHandler::handle');
+            Event::register(self::EVENT_LISTEN, 'KevBaldwyn\NewRelic\EventHandler::transaction');
         }
+
+        Event::register(self::EVENT_APP_BOOT, 'KevBaldwyn\NewRelic\EventHandler::routes');
     }
 
 } 
