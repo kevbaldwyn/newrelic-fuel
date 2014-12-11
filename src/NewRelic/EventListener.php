6c933d7ca6465143898f555b132b874b0da265fc
Kevin Baldwyn <kev@kevbaldwyn.co.uk>
2014-12-11 17:26:43 +0000

    fixed incorrect event name

--- src/NewRelic/EventListener.php
+++ src/NewRelic/EventListener.php
@@ -10,7 +10,7 @@ class EventListener {
     public static function register()
     {
         if (extension_loaded('newrelic')) {
-            Event::register(self::EVENT_LISTEN, 'KevBaldwyn\NewRelic\EventHandler::transaction');
+            Event::register(self::EVENT_TRANSACTION, 'KevBaldwyn\NewRelic\EventHandler::transaction');
         }
 
         EventHandler::routes();
