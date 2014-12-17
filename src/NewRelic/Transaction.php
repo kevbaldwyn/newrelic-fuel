b252c6990d14b3183a7e00ff2136bcae8806ba8a
Kevin Baldwyn <kev@kevbaldwyn.co.uk>
2014-12-17 09:36:05 +0000

    added support for transaction traces for job queues, cron and queue workers

--- src/NewRelic/Transaction.php
+++ src/NewRelic/Transaction.php
@@ -8,10 +8,49 @@ class Transaction {
         }
     }
 
+    public static function addJobQueue($route)
+    {
+        static::end();
+        static::start();
+        static::markAsBackground();
+        static::add($route);
+    }
+
     public static function addParam($key, $value)
     {
         if(extension_loaded('newrelic')) {
             newrelic_add_custom_parameter($key, $value);
+        }
+    }
+
+    public static function markAsBackground()
+    {
+        if(extension_loaded('newrelic')) {
+            newrelic_background_job(true);
+        }
+    }
+
+    public static function start($name = null)
+    {
+        if(extension_loaded('newrelic')) {
+            if(is_null($name)) {
+                $name = ini_get("newrelic.appname");
+            }
+            newrelic_start_transaction($name);
+        }
+    }
+
+    public static function end()
+    {
+        if(extension_loaded('newrelic')) {
+            newrelic_end_transaction();
+        }
+    }
+
+    public static function ignore()
+    {
+        if(extension_loaded('newrelic')) {
+            newrelic_ignore_transaction();
         }
     }
 } 
